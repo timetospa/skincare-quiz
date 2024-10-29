c7c72a9422a862bd089973135abbe354eb2730d5
ade <abruce@thedevelopmentsuite.com>
2024-10-29 12:24:40 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/quiz-test/",
+  base: "/skincare-quiz/",
   plugins: [svelte()],
 })
